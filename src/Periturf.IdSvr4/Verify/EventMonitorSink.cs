b2e0daa14dc12bed30ddfd116055c948b610bb1c
Adam Burton <adz21c@gmail.com>
2019-07-22 23:51:52 +0100

    Ground work for IdentityServer4 conditions.

---
+++ src/Periturf.IdSvr4/Verify/EventMonitorSink.cs
@@ -0,0 +1,56 @@
+﻿/*
+ *     Copyright 2019 Adam Burton (adz21c@gmail.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using IdentityServer4.Events;
+using IdentityServer4.Services;
+
+namespace Periturf.IdSvr4.Verify
+{
+    class EventMonitorSink : IEventMonitorSink
+    {
+        private readonly DefaultEventSink _defaultSink;
+        private readonly ConcurrentDictionary<Type, ConcurrentBag<IEventOccurredConditionEvaluator>> _evaluators = new ConcurrentDictionary<Type, ConcurrentBag<IEventOccurredConditionEvaluator>>();
+
+        public EventMonitorSink(DefaultEventSink defaultSink)
+        {
+            _defaultSink = defaultSink;
+        }
+
+        void IEventMonitorSink.AddEvaluator(Type eventType, IEventOccurredConditionEvaluator evaluator)
+        {
+            var eventTypeEvaluators = _evaluators.GetOrAdd(eventType, x => new ConcurrentBag<IEventOccurredConditionEvaluator>());
+            eventTypeEvaluators.Add(evaluator);
+        }
+
+        void IEventMonitorSink.RemoveEvaluator(Type eventType, IEventOccurredConditionEvaluator evaluator)
+        {
+            throw new NotImplementedException();
+        }
+
+        async Task IEventSink.PersistAsync(Event evt)
+        {
+            await _defaultSink.PersistAsync(evt);
+
+            var eventType = evt.GetType();
+            var eventTypeEvaluators = _evaluators.GetOrAdd(eventType, x => new ConcurrentBag<IEventOccurredConditionEvaluator>());
+
+            foreach (var evaluator in eventTypeEvaluators.ToArray())
+                evaluator.CheckEvent(evt);
+        }
+    }
+}
---
+++ src/Periturf.Tests/IdSvr4/Verify/EventOccurredConditionSpecificationTests.cs
@@ -0,0 +1,50 @@
+﻿/*
+ *     Copyright 2019 Adam Burton (adz21c@gmail.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using FakeItEasy;
+using IdentityServer4.Events;
+using NUnit.Framework;
+using Periturf.IdSvr4.Verify;
+using Periturf.Verify;
+using System;
+using System.Threading.Tasks;
+
+namespace Periturf.Tests.IdSvr4.Verify
+{
+    [TestFixture]
+    class EventOccurredConditionSpecificationTests
+    {
+        [Test]
+        public async Task Given_Spec_When_BuildEvaluator_Then_EvaluatorReturnedAndRegisteredForCheckAndErasePlanned()
+        {
+            var eventMonitorSink = A.Fake<IEventMonitorSink>();
+            var erasePlan = A.Fake<IConditionErasePlan>();
+            var condition = A.Dummy<Func<Event, bool>>();
+
+            IConditionSpecification spec = new EventOccurredConditionSpecification<Event>(eventMonitorSink, condition);
+
+            // Act
+            var evaluator = await spec.BuildEvaluatorAsync(Guid.NewGuid(), erasePlan);
+
+            // Assert
+            Assert.IsNotNull(evaluator);
+            Assert.AreEqual(typeof(EventOccurredConditionEvaluator<Event>), evaluator.GetType());
+            A.CallTo(() => erasePlan.AddEraser(A<IConditionEraser>._)).MustHaveHappened();
+
+            var eventOccurredEvaluator = (IEventOccurredConditionEvaluator)evaluator;
+            A.CallTo(() => eventMonitorSink.AddEvaluator(typeof(EventOccurredConditionEvaluator<Event>), eventOccurredEvaluator)).MustHaveHappened();
+        }
+    }
+}
