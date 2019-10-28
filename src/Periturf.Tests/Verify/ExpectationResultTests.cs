00196f1618cce35d2a15028cc62a3b667e9b60a1
Adam Burton <adz21c@gmail.com>
2019-10-28 21:43:24 +0000

    Expectation Result Coverage

---
+++ src/Periturf.Tests/Verify/ExpectationResultTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Periturf.Verify;
+
+namespace Periturf.Tests.Verify
+{
+    [TestFixture]
+    class ExpectationResultTests
+    {
+        [TestCase(false)]
+        [TestCase(true)]
+        public void Given_Params_When_Ctor_Then_FieldsMapped(bool met)
+        {
+            var result = new ExpectationResult(met);
+
+            Assert.AreEqual(met, result.Met);
+        }
+    }
+}
