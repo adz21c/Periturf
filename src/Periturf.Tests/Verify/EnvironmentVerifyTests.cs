c4626947d00f8a2209056d4a9b4a6fc7f6d7bb8d
Adam Burton <adz21c@gmail.com>
2019-11-02 19:14:36 +0000

    Environment level tests

---
+++ src/Periturf.Tests/Verify/EnvironmentVerifyTests.cs
@@ -0,0 +1,121 @@
+﻿/*
+ *     Copyright 2019 Adam Burton (adz21c@gmail.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using FakeItEasy;
+using NUnit.Framework;
+using Periturf.Components;
+using Periturf.Verify;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Periturf.Tests.Verify
+{
+    [TestFixture]
+    class EnvironmentVerifyTests
+    {
+        private const string ComponentName = "ComponentName";
+        
+        private IExpectationCriteriaEvaluator _expectationCriteria;
+        private IExpectationCriteriaSpecification _expectationSpecification;
+        private Environment _environment;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Expectation
+            _expectationCriteria = A.Fake<IExpectationCriteriaEvaluator>();
+            A.CallTo(() => _expectationCriteria.Completed).Returns(true);
+
+            var expectationCriteriaFactory = A.Fake<IExpectationCriteriaEvaluatorFactory>();
+            A.CallTo(() => expectationCriteriaFactory.CreateInstance()).Returns(_expectationCriteria);
+
+            _expectationSpecification = A.Fake<IExpectationCriteriaSpecification>();
+            A.CallTo(() => _expectationSpecification.Build()).Returns(expectationCriteriaFactory);
+            A.CallTo(() => _expectationSpecification.Timeout).Returns(TimeSpan.FromMilliseconds(100));
+
+            // Component
+            var componentConditionSpecification = A.Fake<IComponentConditionSpecification>();
+            A.CallTo(() => componentConditionSpecification.BuildAsync(A<CancellationToken>._)).Returns(new MockComponentEvaluator(TimeSpan.FromMilliseconds(10), 5));
+            
+            var componentConditionBuilder = A.Fake<ITestComponentConditionBuilder>();
+            A.CallTo(() => componentConditionBuilder.CreateCondition()).Returns(componentConditionSpecification);
+
+            var component = A.Fake<IComponent>();
+            A.CallTo(() => component.CreateConditionBuilder<ITestComponentConditionBuilder>()).Returns(componentConditionBuilder);
+
+            // Environment
+            var host1 = A.Fake<IHost>();
+            A.CallTo(() => host1.Components).Returns(new ReadOnlyDictionary<string, IComponent>(new Dictionary<string, IComponent> { { ComponentName, component } }));
+
+            _environment = Environment.Setup(x =>
+            {
+                x.Host(nameof(host1), host1);
+            });
+        }
+
+        [Test]
+        public async Task Given_MetCondition_When_Verify_Then_VerifyExpectationsMet()
+        {
+            // Arrange
+            var verifier = await _environment.VerifyAsync(c =>
+                c.Expect(
+                    c.GetComponentConditionBuilder<ITestComponentConditionBuilder>(ComponentName).CreateCondition(),
+                    e => e.Must(_expectationSpecification)));
+            A.CallTo(() => _expectationCriteria.Met).Returns(true);
+
+            // Act
+            var result = await verifier.VerifyAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsTrue(result.ExpectationsMet);
+        }
+
+        [Test]
+        public async Task Given_FalseCondition_When_VerifyAndThrowAsync_Then_ThrowsException()
+        {
+            // Arrange
+            var verifier = await _environment.VerifyAsync(c =>
+                c.Expect(
+                    c.GetComponentConditionBuilder<ITestComponentConditionBuilder>(ComponentName).CreateCondition(),
+                    e => e.Must(_expectationSpecification)));
+            A.CallTo(() => _expectationCriteria.Met).Returns(false);
+
+            // Act
+            var result = await verifier.VerifyAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsFalse(result.ExpectationsMet);
+        }
+
+        [Test]
+        public void Given_Environment_When_VerifyWithWrongComponentName_Then_ThrowsException()
+        {
+            // Arrange
+            const string wrongComponentName = "WrongComponentName";
+
+            // Act & Assert
+            var exception = Assert.ThrowsAsync<ComponentLocationFailedException>(() => _environment.VerifyAsync(c =>
+                c.Expect(
+                    c.GetComponentConditionBuilder<ITestComponentConditionBuilder>(wrongComponentName).CreateCondition(),
+                    e => e.Must(_expectationSpecification))));
+            Assert.AreEqual(wrongComponentName, exception.ComponentName);
+        }
+    }
+}
