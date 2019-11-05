d9a60531daacc0778e837ab743893b8466753cee
Adam Burton <adz21c@gmail.com>
2019-11-05 22:43:17 +0000

    Removed code smells

--- src/Periturf/Verify/IVerifier.cs
+++ src/Periturf/Verify/IVerifier.cs
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-using Periturf.Verify;
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
