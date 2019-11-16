9eccafb1460bfefe2095a52ff0374053a8733c06
Adam Burton <adz21c@gmail.com>
2019-11-16 15:00:11 +0000

    Code smell

--- src/Periturf.IdSvr4/Verify/EventOccurredConditionEvaluator.cs
+++ src/Periturf.IdSvr4/Verify/EventOccurredConditionEvaluator.cs
@@ -15,7 +15,6 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using IdentityServer4.Events;
