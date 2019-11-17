115f57b6ad3382fb75205ca27895d171c9e4e8aa
Adam Burton <adz21c@gmail.com>
2019-11-17 22:49:44 +0000

    Code smell cleanup

--- src/Periturf.AspNetCore/Setup/WebHostEnvironmentExtensions.cs
+++ src/Periturf.AspNetCore/Setup/WebHostEnvironmentExtensions.cs
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-using Periturf.AspNetCore;
 using Periturf.AspNetCore.Setup;
 using System;
 using System.Diagnostics.CodeAnalysis;
